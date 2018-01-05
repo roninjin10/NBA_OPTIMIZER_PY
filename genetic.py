31c000f74f292b337d5f40fefeb895e8ed213d47
William Cory <willcory10@gmail.com>
2018-01-05 03:34:47 -0500

    Upload projections from CSV and set up dataframe

--- genetic.py
+++ genetic.py
@@ -1,5 +1,40 @@
 import pandas as pd
 
-projection_df = pd.read_csv('./projections.csv')
-print(projection_df.head())
+dk = 'DRAFT_KINGS'
 
+def roster_constr(site):
+	if site == dk:
+		return ([
+			['PG'],
+			['SG'],
+			['SF'],
+			['PF'],
+			['C'],
+			['PG','SG'],
+			['SF','PF'],
+			['PG','SG','SF','PF','C'],
+			])
+
+sal_cap_dk = 50000
+
+def value(site,projection,salary):
+	if site == dk:
+		min_proj, value_mult, min_sal = 18, 4.2, 3000
+	return projection - (min_proj + value_mult * (salary - min_sal)/1000)
+
+def create_pool(site):
+	out = pd.read_csv('./projections.csv')[['Name','Projection','Salary','Position','Team']]
+	
+	out['Value'] = value(site,out['Projection'],out['Salary'])
+	for i, roster_spot in enumerate(roster_constr(site)):
+		for roster_position in roster_spot:
+			out["isEligibleRosterSpot" + str(i)] = True if roster_position in out['Position'] else False
+	out["isExcluded"] = False
+	out["isLocked"] = True
+	out["isInLineup"] = False
+	
+	return out
+
+if __name__ == "__main__":
+	player_pool = create_pool(dk)
+	print(player_pool.head())
