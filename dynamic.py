f7844afed4bcad3e40827a15bc6390d6226455cb
William Cory <willcory10@gmail.com>
2018-01-18 15:56:12 -0500

    Somewhat finished going to refactor with inheritence

--- dynamic.py
+++ dynamic.py
@@ -136,34 +136,55 @@ class Lineup:
         out.roster = self.roster
 		return out
 
+
 class Pool:
 
-	def __init__(self):
-        pool = []
-		__orig_pool = pd.DataFrame({})
+    def __init__(self):
+        __pool = []
+        __orig_pool = pd.DataFrame({})
 
-	def from_csv(self, site):
-
-		__orig_pool = pd.read_csv('./projections.csv')[['Name','Projection','Salary','Position','Team']]
-		
-		__orig_pool['Value'] = value(site,__orig_pool['Projection'],__orig_pool['Salary'])
-
-		for i, roster_spot in enumerate(roster_constr(site)):
-			out["isEligibleRosterSpot" + str(i)] = False
-			for roster_position in roster_spot:
-				__orig_pool["isEligibleRosterSpot" + str(i)] = np.logical_or(__orig_pool['Position'].str.contains(roster_position) , __orig_pool["isEligibleRosterSpot" + str(i)])
-		
-		__orig_pool["isExcluded"] = False
-		__orig_pool["isLocked"] = False
-		__orig_pool["isInLineup"] = False
-
-	def __create_pool(self):
-		for index, row in __orig_pool.interrows():
+    def __init_pool(self):
+    	for index, row in __orig_pool.interrows():
 			new_player = Player(name=row['Name'], team=row['Team'], opp=None, position=(None, None), projection=None, salary=None, real_value = None, site_id = None)
-		
 
-	def dynamic_optimize(self, lineup):
-		pass
+    def create_pool_from_csv(self, csv):
+        __orig_pool = pd.read_csv('./projections.csv')[['Name','Projection','Salary','Position','Team']]
+        __init_pool(self)
+
+    def create_positional_list(self, pos):
+        #creates a list of players eligible for a specific position. PG, SG, SF, PF, C, G, F, Flex
+        out = {}
+        if pos == 'f':
+            return [player for player in self.__pool if 'sf' in player.get_info('position') or 'pf' in player.get_info('position')]
+        elif pos == 'g':
+            return [player for player in self.__pool if 'pg' in player.get_info('position') or 'sg' in player.get_info('position')]
+        else:
+            return [player for player in self.__pool if pos in player.get_info('position')]
+
+    def remove_player(self, player_name='', index=-1):
+        #removes player from a list
+        if index != -1:
+            removed_player =__pool.pop(index)
+        elif player_name != '':
+            removed_player = __pool.pop(__pool.index(player_name))
+
+    def pop_player(self, player_name='', index=-1):
+        #removes player from a list and returns that player
+        if index != -1:
+            return __pool.pop(index)
+        elif player_name != '':
+            return __pool.pop(__pool.index(player_name))
+
+    def add_player(self, player_name):
+        #adds a player to the player_pool not necessary atm
+        pass
+
+    def restore_player_pool(self):
+        self.__pool = self.__orig_pool
+
+    def get_player_index(self, current_position, current_player_num):
+        pass
+        #gets the player index in __pool based on current position and player_num at position
 
 if __name__ == "__main__":
 	player_pool = Pool()
