8203c4724e9f4cbf784a7c0ef71d932e40adca99
William Cory <willcory10@gmail.com>
2018-01-18 17:33:17 -0500

    moved draftkings class to constants

--- constants.py
+++ constants.py
@@ -45,3 +45,19 @@ dk_sal_cap = 50000
 dk_minimum_proj = 18
 dk_val_mult = 4.2
 dk_min_sal = 3000
+
+class DraftKings:
+
+    def __init_(self)
+        self.name = name
+        self.roster_construction = dk_roster_constr
+        self.sal_cap = dk_sal_cap
+        self.minimum_proj = dk_minimum_proj
+        self.value_mult = dk_val_mult
+        self.min_sal = dk_min_sal
+
+    def value(self,projection,salary):
+        return projection - (self.min_proj + self.value_mult * (salary - self.min_sal)/1000)
+
+    def empty_roster():
+        return dk_empty_roster
