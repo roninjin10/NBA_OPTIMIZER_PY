cdc9fed89bf41e5fbb489578e15ac05f99381f1e
William Cory <willcory10@gmail.com>
2018-01-18 15:35:00 -0500

    Finished but not tested Lineup Class

--- dynamic.py
+++ dynamic.py
@@ -73,39 +73,37 @@ class Lineup:
 
         lineup_site = Site(site)
         
-        self.roster = []
+        self.roster = {}
         for i in range(len(Site.roster_construction)):
-        	self.roster.append(None)
+        	self.roster[i] = None
 
         self.salary_cap = Site.salary_cap
 
     def salary(self):
-        return sum(plr.salary for plr in self.roster if not plr is None)
+        return sum(plr.salary for plr in self.roster.items() if not plr is None)
 
     def points(self):
         return sum(plr.projection for plr in self.roster.items() if not plr is None)
 
     def create_key(self, current_position, current_player_num):
-        return "Pos: " + current_position + " PlNum: " + current_player_num + " Sal: " + str(self.salary()) + " Positions: " + ''.join(pos for pos in self.roster.keys() if not self.roster[pos] is None)
+        return "Pos: " + current_position + " PlNum: " + current_player_num + " Sal: " + str(self.salary()) + " Positions: " + ''.join(str(pos != None) for pos in self.roster.items())
 
 
     def merge_lineup(self, lineup):
         for pos in self.roster.keys():
-            if self[pos] = None:
-                self[pos] = lineup[pos]
+        	if self[pos] = None:
+        		self[pos] = lineup[pos]
 
     def erase_lineup(self):
-        self.roster = {}
+        for pos in self.roster.keys():
+        	self[pos] = None
 
-    def add_player(self, new_player, roster_spot=''):
+    def add_player(self, new_player, roster_spot=None):
         #adds a player to a lineup to a specified roster_spot. Overwrites player if roster_spot specified
         # If roster_spot is not given, it puts the player in the valid, empty roster spot that provides most future roster flexibility
         # method returns a boolean based on if the player was added or not
-        if roster_spot != '':
+        if roster_spot != None:
             self.roster[roster_spot] = new_player
-        else:
-            position = new_player.get_info('position')
-            #don't need this functionality atm
 
 
     def is_valid_salary(self):
@@ -113,19 +111,19 @@ class Lineup:
 
     def is_valid_team(self):
         team_opponent = []
-        for position in self.roster.items():
+        for plr in self.roster.items():
             if team_opponent == []:
-                team_opponent.append(position.get_info('team'))
-                team_opponent.append(position.get_info('opp'))
+                team_opponent.append(plr.get_info('team'))
+                team_opponent.append(plr.get_info('opp'))
             else:
-                if not position.get_info('team') in team_opponent:
+                if not plr.get_info('team') in team_opponent:
                     return True
         return False
 
     def is_valid_positional(self):
         #returns boolean based on if the lineup has every position filled
-        for roster_spot in self.roster.items():
-            if roster_spot is None:
+        for plr in self.roster.items():
+            if plr is None:
                 return False
         return True
 
