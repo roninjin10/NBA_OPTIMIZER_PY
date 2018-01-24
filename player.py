7143f87c020eff25da61a8350ce8f777358c58d6
William Cory <willcory10@gmail.com>
2018-01-24 14:20:03 -0500

    fixed projections to dfs_projections

--- player.py
+++ player.py
@@ -1,17 +1,17 @@
 import constants
-from site import Site 
+from dfs_site import DFS_Site 
 
 class Player:
 
-    def __init__(self, name, team, opp, position, salary, site_id, projection, site= constants.dk):
+    def __init__(self, name, team, opp, position, salary, dfs_site_id, stat_projections, dfs_site= constants.dk):
         self.name = name
         self.team = team
         self.opp = opp
         self.position = position
         self.salary = salary
-        self.__site_id = site_id
-        self.projection = projection 
-        self.site = Site(site)
+        self.__dfs_site_id = dfs_site_id
+        self.stat_projections = stat_projections
+        self.dfs_site = DFS_Site(dfs_site)
         
         self.__test_player()
 
@@ -19,11 +19,11 @@ class Player:
         """test to make sure it is a valid player"""
         pass
 
-    def projection(self):
-        return self.site.fantasy_points(self.projection)
+    def dfs_projection(self):
+        return self.dfs_site.fantasy_points(self.stat_projections)
 
-    def site_id(self):
-        return __site_id[self.site.site]
+    def dfs_site_id(self):
+        return __site_id[self.dfs_site.dfs_site]
 
     def value(self):
-        return self.site.value(self.projection)
+        return self.dfs_site.value(self.projection)
--- test_optimizer.py
+++ test_optimizer.py
@@ -1,17 +1,19 @@
 from constants import *
 from player import Player
+import unittest
+
 
 class TestPlayer(unittest.TestCase):
 
     def setUp(self):
-        self.test_player = Player(name='name', team='team', opp='opp', position='pos', salary=3000, site_id ={dk: '152334'}, projection={'pts' : 10, 'rbs': 1, 'asts': 1, 'stls': 1, 'blks': 1, 'tos': 1, 'threes': 1}, site=dk)
+        self.test_player = Player(name='name', team='team', opp='opp', position='pos', salary=3000, dfs_site_id ={dk: '152334'}, projection={'pts' : 10, 'rbs': 1, 'asts': 1, 'stls': 1, 'blks': 1, 'tos': 1, 'threes': 1}, dfs_site=dk)
         
 
     def test_player(self):
         pass
 
     def test_projection(self):
-        self.assertEqual(self.test_player.projection(), 16.75)
+        self.assertEqual(self.test_player.dfs_projection(), 16.75)
 
     def test_value(self):
         self.assertEqual(self.test_player.value(), -1.25)
