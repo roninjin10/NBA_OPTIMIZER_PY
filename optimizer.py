00db28872cfe2fa6fbfa5073d38be55eae474550
William Cory <willcory10@gmail.com>
2018-01-29 16:17:10 -0500

    completed but did not test dynamic optimizer

--- optimizer.py
+++ optimizer.py
@@ -1,50 +1,76 @@
 import copy
+import constants
+from dfs_site import DFS_Site
+from lineup import Lineup
+from player import Player
+from pool import Pool
 
 class __Optimizer:
 
-	def __init__(self, site=dk, pool=None, lineups=[]):
-		self.site = Site(site)
-		self.pool = pool
-		self.lineups = lineups
-		self.memoized = {}
+    def __init__(self, pool=None, site=constants.dk, lineups=[]):
+        self.site = Site(site)
+        self.pool = pool
+        self.lineups = lineups
+        self.memoized = {}
 
-		if self.pool == None:
-			pool = Pool()
+        if self.pool == None:
+            pool = Pool()
 
-        def __len__(self):
-            return len(lineups)
+    def __len__(self):
+        return len(lineups)
 
-        def __repr__(self):
-            return repr(lineups)
+    def __repr__(self):
+        return repr(lineups)
 
-        def __getitem__(self, key):
-            return lineups[key]
+    def __getitem__(self, key):
+        return lineups[key]
 
 
 class Dynamic_Optimizer(__Optimizer):
 
-	def __recursive_step(self, cur_lineup, cur_position, cur_player):
-		if cur_lineup.is_valid_lineup:
-			return cur_lineup
+    def __recursive_step(self, pool, lu):
+        self.recursion_counter += 1
+        print(self.recursion_counter)
+
+        if len(lu) == len(self.site.roster_construction)
+            return lu
+
+        if pool.current_index == len(pool):
+            return None
+
+        lu_key = lu.dic_key(pool.current_position, pool.positional_index)
+        if lu_key in self.memoized.keys():
+            return lu.merge_lineups(self.memoized[lu_key])
+
+        lu_take = copy.deepcopy(lu)
+        take_player = pool.current_player()
+        pool.next_player()
+        if lu_take.add_player(take_player) == 0:
+            lu_take = self.__recursive_step(pool,lu_take)
+        else:
+            lu_take = None
+
+        lu_pass = copy.deepcopy(lu)
+        lu_pass = self.__recursive_step(pool,lu_pass)
+
+        if lu_pass == None or lu_take == None:
+            return lu_take if lu_pass == None else lu_take
+        else:
+            return lu_take if lu_take.dfs_projection() >= lu_pass else lu_pass
+        
 
 
-		lineup_key = cur_lineup.key(cur_position, cur_player)
-		if lineup_key in self.memoized.keys:
-			return cur_lineup.merge_lineup(self.memoized[lineup_key])
+    def optimize(self, n=1):
+        self.recursion_counter = 0
+        
+        lu = Lineup(site_name=constants.dk)
+        pool = self.pool
+        
+        opt = Optimizer(pool, lu)
 
-		lineup_if_pass = __dynamic_optimizer(copy.deepcopy(cur_lineup), cur_position, cur_player + 1)
-
-		lineup_if_take = __dynamic_optimizer(copy.deepcopy(cur_lineup).add_player(self.lineups[cur_player]), cur_position + 1, 0)
-
-		if lineup_if_pass == None or lineup_if_take == None:
-			return lineup_if_pass if lineup_if_take == None else lineup_if_take
-		return lineup_if_pass if lineup_if_pass.salary > lineup_if_take.salary else lineup_if_take
-
-	def optimize(self, n):
-		self.memoized = {}
-		self.lineups.append( __dynamic_optimizer(empty_roster(self.site), 0, 0))
 
 class Genetic_Optimizer(__Optimizer):
 
-	def optimize(self, n):
-            pass
+    def optimize(self, n):
+        pass
+
